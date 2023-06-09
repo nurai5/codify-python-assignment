6da374c01046dc940f93ac07e85ccd1046f2d02e
nurai5 <134472282+nurai5@users.noreply.github.com>
2023-06-09 18:35:43 +0600

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/', include('chat_api.urls')),
 ]
 
